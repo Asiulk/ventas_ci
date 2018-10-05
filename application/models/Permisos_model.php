6ef265aef25cba9d4b1754e04e157d0b3b7b45d4
Practicante Sistemas Medellín2 <prtsistmed2@UNIBAN.COM.CO>
2018-10-05 17:02:19 -0500

    Ya casi 7u7

--- application/models/Permisos_model.php
+++ application/models/Permisos_model.php
@@ -2,12 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Permisos_model extends CI_Model {
-  /*public function getPermisos(){
+    public function getPermisos(){
     $this->db->select("p.*,m.nombre as menu, r.nombre as rol");
     $this->db->from("permisos p");
     $this->db->join("roles r","p.rol_id = r.id");
     $this->db->join("menus m","p.menu_id = m.id");
-    $result = $this->db->get();
+    $resultados = $this->db->get();
     return $resultados->result();
-  }*/
+  }
 }
