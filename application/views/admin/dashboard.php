511c84022ab4039217f88260299d474b9850890c
estibenlicona <estibenlicona@gmail.com>
2018-10-09 06:42:49 -0500

    licona

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -2,3 +2,23 @@
         <!-- =============================================== -->
 
         <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                Categoria
+                <small>Editar</small>
+                </h1>
+            </section>
+            <!-- Ma            <section class="content">
+                <!-- Default box -->
+                <div class="box box-solid">
+                    <div class="box-body">
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
